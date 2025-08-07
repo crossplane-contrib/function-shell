8a558cd5b5308cd3d3898c47d57d42ffa347f1e6
Steven Borrelli <steve@borrelli.org>
2025-08-07 17:56:21 +0100

    ignore message in error due to local shell differences
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- fn_test.go
+++ fn_test.go
@@ -197,7 +197,7 @@ func TestRunFunction(t *testing.T) {
 			f := &Function{log: logging.NewNopLogger()}
 			rsp, err := f.RunFunction(tc.args.ctx, tc.args.req)
 
-			if diff := cmp.Diff(tc.want.rsp, rsp, protocmp.Transform()); diff != "" {
+			if diff := cmp.Diff(tc.want.rsp, rsp, protocmp.Transform(), cmpopts.IgnoreFields(fnv1.Result{}, "Message")); diff != "" {
 				t.Errorf("%s\nf.RunFunction(...): -want rsp, +got rsp:\n%s", tc.reason, diff)
 			}
 
